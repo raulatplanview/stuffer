4301b729d2ba5993be0c90d8e19dd8f5a118b86f
Raul Reyes <rreyes@planview.com>
2021-01-27 12:38:44 -0600

    pushing authentication changes

--- Stuffer.ps1
+++ Stuffer.ps1
@@ -1,25 +1,11 @@
-<# ADMIN #>
-$admin = "rreyes"
-
 <# STUFFER PATH #>
 $stufferDirectory = $MyInvocation.MyCommand.Path | Split-Path
 
-<# TO LOCATION #>
-$stufferType = Read-Host "Where is the environment located: 1-AWS or 2-US/EU"
+<# PRIMARY CUSTOMER INFORMATION #>
+$option = Read-Host "Select a region: 1-SG or 2-LN"
+$customerName = Read-Host "Enter the customer OU name"
+$customerCode = Read-Host "Enter the customer code"
 
-if ($stufferType -eq 1) {
-    
-    $customerCode = Read-Host "Enter the customer code"
-    $option = Read-Host "Select a region: 1-Frankfurt or 2-Sydney"
+. "$($stufferDirectory)\Locations\US-EU.ps1" 
 
-    . "$($stufferDirectory)\Locations\AWS.ps1" 
 
-}
-else {
-
-    $option = Read-Host "Select a region: 1-SG or 2-LN"
-    $customerName = Read-Host "Enter the customer OU name"
-    $customerCode = Read-Host "Enter the customer code"
-
-    . "$($stufferDirectory)\Locations\US-EU.ps1" 
-}
